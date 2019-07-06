e55d8ecb5c365a8f3629a1e9325ddd00e6bdbdc7
Krizza Bullecer <krizzabullecer@gmail.com>
2019-07-07 02:30:15 +0800

    Updated choose character

--- application/models/Item_Model.php
+++ application/models/Item_Model.php
@@ -2,6 +2,14 @@
 
 class Item_Model extends CI_Model {
 
+    public function fetch($table, $where = NULL) {
+        if (!empty($where)) {
+            $this->db->where($where);
+        }
+        $query = $this->db->get($table);
+        return ($query->num_rows()) ? $query->result() : false;
+    }
+
     public function insertData($table, $data) {
         $this->db->insert($table, $data);
         return $this->db->affected_rows();
