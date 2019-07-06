655b15c54279a9e97d6bc1010c0a18e554dd05fd
Krizza Bullecer <krizzabullecer@gmail.com>
2019-07-06 15:39:49 +0800

    Update style

--- application/views/landing_page.php
+++ application/views/landing_page.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<title>Team iKARLy</title>
     <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
-    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" type="text/css" href="<?=base_url()?>assets/css/style.css">
 </head>
 
 <body>
