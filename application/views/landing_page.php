d6518033acd0a5ad58d51c1523b11d8cc8e686fc
Krizza Bullecer <krizzabullecer@gmail.com>
2019-07-06 14:16:34 +0800

    Updated CodeIgniter Welcome Page

---
+++ application/views/landing_page.php
@@ -0,0 +1,16 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Team iKARLy</title>
+</head>
+<body>
+
+<div id="container">
+
+</div>
+
+</body>
+</html>
