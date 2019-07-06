26a6d083c4f11ad337084d901767c9c1d0059ebe
Krizza Bullecer <krizzabullecer@gmail.com>
2019-07-06 15:06:08 +0800

    Added skeleton

--- application/views/landing_page.php
+++ application/views/landing_page.php
@@ -1,17 +1,78 @@
-<?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-?><!DOCTYPE html>
+<!DOCTYPE html>
 <html lang="en">
 <head>
 	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<title>Team iKARLy</title>
+    <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
+    <link rel="stylesheet" type="text/css" href="style.css">
 </head>
+
 <body>
+    <div id="app" style="text-align: center;">
+        <div id="landing" class="container">
+            <h1>title here</h1>
+            <button>start</button>
+        </div>
+        <div id="characters">
+            <section id="international">
+                <h1>Local</h1>
+                <div class="char-row">
+                    <div class="char-box"></div>
+                    <div class="char-box"></div>
+                    <div class="char-box"></div>
+                    <div class="char-box"></div>
+                    <div class="char-box"></div>
+                </div>
+            </section>
+            <section id="local">
+                <h1>International</h1>
+                <div class="char-row">
+                    <div class="char-box"></div>
+                    <div class="char-box"></div>
+                    <div class="char-box"></div>
+                    <div class="char-box"></div>
+                    <div class="char-box"></div>
+                </div>
+            </section>
+            <button>play</button>
+        </div>
+        <div id="in-game">
+            <section id="board-game">
+                <div id="box1"></div>
+                <div id="box2"></div>
+                <div id="box3"></div>
+                <div id="box4"></div>
+                <div id="box5"></div>
+                <div id="box6"></div>
+                <!-- END OF ROW 1 -->
+                <div id="box7"></div>
+                <div id="box8"></div>
+                <div id="box9"></div>
+                <div id="box10"></div>
+                <div id="box11"></div>
+                <div id="box12"></div>
+                <!-- END OF ROW 2 -->
+                <div id="box13"></div>
+                <div id="box14"></div>
+                <div id="box15"></div>
+                <div id="box16"></div>
+                <div id="box17"></div>
+                <div id="box18"></div>
+                <!-- END OF ROW 3 -->
+                <div id="box19"></div>
+                <div id="box20"></div>
+                <div id="box21"></div>
+                <div id="box22"></div>
+                <div id="box23"></div>
+                <div id="box24"></div>
+                <!-- END OF ROW 4 -->
+            </section>
+        </div>
+    </div>
 
-<div id="container" style="text-align: center;">
-    <h1>title here</h1>
-    <button>start</button>
-</div>
+    <script>
 
+    </script>
 </body>
 </html>
